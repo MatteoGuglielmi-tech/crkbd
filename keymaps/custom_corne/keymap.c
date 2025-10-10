dc594f39ee014a48466b851d03211792a01bdbd2
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2025-10-10 21:38:02 +0200

    feat(layout): add volume controls, refactor

--- keymaps/custom_corne/keymap.c
+++ keymaps/custom_corne/keymap.c
@@ -1,8 +1,13 @@
 #include QMK_KEYBOARD_H
 
-#define HYPER_SPC MT(MOD_LCTL|MOD_LALT, KC_SPC)
-#define HYPER_TAB MT(MOD_LCTL|MOD_LALT|MOD_LSFT, KC_TAB)
-#define HYPER_QUOTE MT(MOD_LALT|MOD_LSFT, KC_QUOT)
+#define VOL_UP   KC_KB_VOLUME_UP
+#define VOL_DOWN KC_KB_VOLUME_DOWN
+
+#define HP_SPC MT(MOD_LCTL|MOD_LALT, KC_SPC)
+#define HP_TAB MT(MOD_LCTL|MOD_LALT|MOD_LSFT, KC_TAB)
+#define HP_QUOTE MT(MOD_LALT|MOD_LSFT, KC_QUOT)
+
+#define TD_CLN TD(TD_SCLN_COLN)
 
 enum layers { _DEFAULT, _LOWER, _RAISE, _ADJUST };
 enum custom_keycodes { TD_SCLN_COLN };
@@ -12,36 +17,36 @@ tap_dance_action_t tap_dance_actions[] = { [TD_SCLN_COLN] = ACTION_TAP_DANCE_DOU
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEFAULT] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
+  //,------------------------------------------------------                    ,------------------------------------------------------
+      KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      HYPER_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L, TD(TD_SCLN_COLN), HYPER_QUOTE,
+      HP_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    TD_CLN,  HP_QUOTE,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RGUI,
+      KC_LSFT,    KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RGUI,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                   KC_LCTL,   MO(_LOWER),  HYPER_SPC,     KC_ENT,   MO(_RAISE), KC_RALT
+                                   KC_LCTL,   MO(_LOWER),  HP_SPC,     KC_ENT,   MO(_RAISE), KC_RALT
                                //|----------------------------------| |---------------------------------|
   ),
 
     [_LOWER] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_DEL,
+  //,------------------------------------------------------                    ,------------------------------------------------------
+      KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      HYPER_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,     KC_0,  KC_CAPS,
+      HP_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_CAPS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_PGUP,  KC_PGDN,
+      KC_LSFT, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE, XXXXXXX,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_PGUP,  KC_PGDN,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCTL, _______, HYPER_SPC,     KC_ENT,   MO(_RAISE), KC_RALT
+                                          KC_LCTL, _______, HP_SPC,     KC_ENT,   MO(_RAISE), KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 
     [_RAISE] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+  //,------------------------------------------------------                    ,------------------------------------------------------
       KC_ESC, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, VOL_UP, VOL_DOWN,                     KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LCTL,   MO(_ADJUST),  KC_SPC,     KC_ENT, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
