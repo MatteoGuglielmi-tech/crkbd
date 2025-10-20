e213f02bf1cbd1025fbceb01f5cb3998d4d508df
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2025-10-20 16:08:28 +0200

    feat(tap dances): add

--- keymaps/custom_corne/keymap.c
+++ keymaps/custom_corne/keymap.c
@@ -5,14 +5,24 @@
 
 #define HP_SPC MT(MOD_LCTL|MOD_LALT, KC_SPC)
 #define HP_TAB MT(MOD_LCTL|MOD_LALT|MOD_LSFT, KC_TAB)
-#define HP_QUOTE MT(MOD_LALT|MOD_LSFT, KC_QUOT)
+// #define HP_QUOTE MT(MOD_LALT|MOD_LSFT, KC_QUOT)
 
 #define TD_CLN TD(TD_SCLN_COLN)
+#define TD_SLSH TD(TD_SLSH_QUESTMARK)
+#define TD_COMM TD(TD_COMM_LT)
+#define TD_DOT TD(TD_DOT_GT)
+#define TD_QUOTE TD(TD_SQUOTE_DQUOTES)
 
 enum layers { _DEFAULT, _LOWER, _RAISE, _ADJUST };
-enum custom_keycodes { TD_SCLN_COLN };
+enum custom_keycodes { TD_SCLN_COLN, TD_SLSH_QUESTMARK, TD_COMM_LT, TD_DOT_GT, TD_SQUOTE_DQUOTES };
 
-tap_dance_action_t tap_dance_actions[] = { [TD_SCLN_COLN] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_COLN) };
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_SCLN_COLN] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_COLN),
+    [TD_SLSH_QUESTMARK] = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, KC_QUES),
+    [TD_COMM_LT] = ACTION_TAP_DANCE_DOUBLE(KC_COMM, KC_LT),
+    [TD_DOT_GT] = ACTION_TAP_DANCE_DOUBLE(KC_DOT, KC_GT),
+    [TD_SQUOTE_DQUOTES] = ACTION_TAP_DANCE_DOUBLE(KC_QUOTE, KC_DQT),
+};
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -20,9 +30,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,------------------------------------------------------                    ,------------------------------------------------------
       KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      HP_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    TD_CLN,  HP_QUOTE,
+      HP_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    TD_CLN,  TD_QUOTE,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RGUI,
+      KC_LSFT,    KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,    KC_M,    TD_COMM, TD_DOT,  TD_SLSH, KC_RGUI,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                    KC_LCTL,   MO(_LOWER),  HP_SPC,     KC_ENT,   MO(_RAISE), KC_RALT
                                //|----------------------------------| |---------------------------------|
@@ -34,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       HP_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_CAPS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE, XXXXXXX,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_PGUP,  KC_PGDN,
+      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_PGUP, KC_PGDN,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LCTL, _______, HP_SPC,     KC_ENT,   MO(_RAISE), KC_RALT
                                       //`--------------------------'  `--------------------------'
