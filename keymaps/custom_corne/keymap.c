af12c36b3aa38bbfedfa20309130f840dce4eea1
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2025-10-22 13:30:19 +0200

    build(keymap): remove unused bindings

--- keymaps/custom_corne/keymap.c
+++ keymaps/custom_corne/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       HP_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_CAPS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_PGUP, KC_PGDN,
+      KC_LSFT, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE, XXXXXXX,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_PGUP,  KC_PGDN,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LCTL, _______, HP_SPC,     KC_ENT,   MO(_RAISE), KC_RALT
                                       //`--------------------------'  `--------------------------'
